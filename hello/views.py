9f79f068d535eb449b2b098333450d5a26404bae
christyjoe <acjpjoe@gmail.com>
2021-03-20 19:27:41 +0530

    starting

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
+def hello(request):
     return HttpResponse("Hello, world. You're CJ's new website.")
